f0b5b2047dbdd557fcb012b31596c70d2283ae75
Abdullah jubayer <abdullahjubayer38@gmail.com>
2020-07-08 09:32:12 +0600

    8/7/2020

--- app/src/main/java/com/fci/androCroder/BD/Service/NetworkUtils.java
+++ app/src/main/java/com/fci/androCroder/BD/Service/NetworkUtils.java
@@ -8,10 +8,6 @@ public class NetworkUtils {
     public static boolean getConnectivityStatus(Context context){
         ConnectivityManager manager= (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo info=manager.getActiveNetworkInfo();
-        if (info != null && info.isConnected()){
-            return true;
-        }else {
-            return false;
-        }
+        return info != null && info.isConnected();
     }
 }
